28c337fafd6e44412c37c764f361faf77bea2db7
Matias Fabian Lecaros Aranis <m.lecaros.aranis@gmail.com>
2013-11-28 10:18:18 -0300

    A lo tortuga ninja. Con movimiento en X e Y, y Z relativo, para tener elementos antes y después. TODO: No pude hacer que el mono choque con la caja.
    Signed-off-by: Matias Fabian Lecaros Aranis <m.lecaros.aranis@gmail.com>

---
+++ Assets/PlayerMovement.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+public var moveSpeed : float = 30F;
+
+function Start () {
+
+}
+
+function Update () {
+	transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime,0,0);
+	transform.Translate(0,Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime,Input.GetAxis("Vertical")*0.5); //lo mueve hacia arriba
+	//y tb en el eje Z, para que hayan elementos antes y despues
+	
+}
