37985a04ee4748174095118a1bc8e5c0433d4d4e
Matias Fabian Lecaros Aranis <m.lecaros.aranis@gmail.com>
2013-12-02 12:39:19 -0300

    Ahora sí, se agrega escena para presentar como prototipo(prueba_02 - Prototipo.unity). Signed-off-by: Matias Fabian Lecaros Aranis <m.lecaros.aranis@gmail.com>

--- Assets/PlayerMovement.js
+++ Assets/PlayerMovement.js
@@ -7,7 +7,9 @@ function Start () {
 
 function Update () {
 	transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime,0,0);
-	transform.Translate(0,Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime,Input.GetAxis("Vertical")*0.5); //lo mueve hacia arriba
+	transform.Translate(0,Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime,Input.GetAxis("Vertical")* moveSpeed * Time.deltaTime); //lo mueve hacia arriba
 	//y tb en el eje Z, para que hayan elementos antes y despues
+	
+	//rigidbody.AddRelativeForce (Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime,0,0, ForceMode.VelocityChange);
 	
 }
