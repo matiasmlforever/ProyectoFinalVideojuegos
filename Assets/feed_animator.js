084234cc7ab43fb027514d1a5fd850b5603f66c1
Matias Lecaros <m.lecaros.aranis@gmail.com>
2013-12-10 19:59:48 -0300

    Pruebas con animación de sprites usando unity, se anima al mono caminando.
    Signed-off-by: Matias Lecaros <m.lecaros.aranis@gmail.com>

---
+++ Assets/feed_animator.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function FixedUpdate () {
+	var ani:Animator;
+	var velocidads:float = Input.GetAxis("Horizontal");
+	ani = GetComponent(Animator);
+	ani.SetFloat("velocidad_mov",Mathf.Abs(velocidads)); 
+}
