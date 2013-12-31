1481bfdddb2a48af23ad67ae7188614c900b7017
Matias Lecaros <m.lecaros.aranis@gmail.com>
2013-12-31 07:30:37 -0300

    PosteLuz.js
    Signed-off-by: Matias Lecaros <m.lecaros.aranis@gmail.com>

---
+++ Assets/JointerBreak.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnJointBreak(breakForce : float) { 
+		var foco:Transform;
+		var luz:Transform;
+		Debug.Log("Joint Broke!, force: " + breakForce);
+		foco = transform.parent.Find("Foco");
+		luz = foco.transform.Find("Point Light");
+		
+		
+		//destruye la luz
+		Destroy(luz);
+}
