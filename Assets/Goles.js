3aa647bd95edc3124dcfc1a0cd4f6853491ea274
Matias Lecaros <m.lecaros.aranis@gmail.com>
2014-01-14 20:20:41 -0300

    Otro avance Stage2, los goles están favoreciendo al equipo local.
    Signed-off-by: Matias Lecaros <m.lecaros.aranis@gmail.com>

---
+++ Assets/Goles.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+public var gol_rival:int=0;
+public var gol_amigo:int=0;
+
+public var buf_rival:int=0;
+public var buf_amigo:int=0;
+
+function Start () {
+	
+}
+
+function Update () {
+	var valoooor:float;
+	valoooor = Random.value;
+	if(valoooor >= 0.9)
+	{
+	 	gol_rival++;
+	 	buf_rival=1;
+	}
+	else
+	{
+		gol_amigo++;
+		buf_amigo=1;
+	}
+	
+	gameObject.GetComponentInChildren(GUIText).guiText.text=gol_rival.ToString()+"-"+gol_amigo.ToString();
+}
---
+++ Assets/TiempoPartido.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+//public var erdeltatiempo:float;
+//public var ertiempo:float;
+public var aceleracion:float=1;
+
+public var minutos:int=0;
+public var segundos:int=0;
+public var segundos_totales:int=0;
+
+function Start () {
+
+}
+
+function Update () {
+	//segundos_totales++;
+	segundos = segundos + aceleracion* Mathf.CeilToInt(Time.deltaTime);
+	//erdeltatiempo = Time.deltaTime;
+	//ertiempo = Time.timeSinceLevelLoad;
+	//minutos = Mathf.FloorToInt((ertiempo/60) * aceleracion);
+	//segundos = Mathf.FloorToInt(ertiempo - (minutos * 60));
+	
+	//segundos_totales = Mathf.FloorToInt(aceleracion*(segundos_totales - (minutos * 60)));
+	if(segundos >= 60)// && minutos!=0)
+	{
+		segundos = 0;	
+		minutos++;
+	}
+	
+	//segundos = Mathf.FloorToInt(segundos_totales - (minutos * 60));
+	Debug.Log(gameObject.GetComponentInChildren(GUIText));
+	
+	gameObject.GetComponentInChildren(GUIText).guiText.text=minutos.ToString()+":"+segundos.ToString();
+}
